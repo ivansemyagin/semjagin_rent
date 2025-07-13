a4ed4ca87dca1b4d9e5d53a19b951c27cf876f9e
ivansemyagin <43599581+ivansemyagin@users.noreply.github.com>
2025-07-14 00:46:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ import requests
 
 def parse_flat_info():
     url = "https://inberlinwohnen.de/wohnungsfinder/"
-    headers = {"User-Agent": "Mozilla/5.0"}
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+        "Accept-Language": "de,en;q=0.9",
+        "Connection": "keep-alive",
+    }
 
     response = requests.get(url, headers=headers)
     html = response.text  # вот это нужно передавать в BeautifulSoup
