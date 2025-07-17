8c1ada4b88a9bea438369469c2cf53833e6627a8
ivansemyagin <43599581+ivansemyagin@users.noreply.github.com>
2025-07-18 01:03:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ import traceback
 import random
 from fake_useragent import UserAgent
 ua = UserAgent()
+import brotli
+from io import BytesIO
 
 # === Telegram config ===
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN", "твой_токен")
@@ -66,6 +68,16 @@ def parse_flat_info(session):
             logging.info(f"Content-Encoding: {response.headers.get('Content-Encoding')}")
             logging.info(f"Content-Type: {response.headers.get('Content-Type')}")
             response.raise_for_status()
+            content_encoding = response.headers.get('Content-Encoding', '')
+            if 'br' in content_encoding:
+                html = brotli.decompress(response.content).decode('utf-8')
+            elif 'gzip' in content_encoding:
+                import gzip
+                buf = BytesIO(response.content)
+                html = gzip.GzipFile(fileobj=buf).read().decode('utf-8')
+            else:
+                html = response.text
+
             break
         except requests.RequestException as e:
             logging.warning(f"Попытка {attempt + 1} не удалась: {e}")
@@ -74,8 +86,8 @@ def parse_flat_info(session):
         logging.error("❌ Не удалось получить страницу после 3 попыток.")
         return []
     
-    logging.info(f"Текст: {response.text}")
-    soup = BeautifulSoup(response.text, "html.parser")
+    logging.info(f"Текст: {html}")
+    soup = BeautifulSoup(html, "html.parser")
     flats = []
     logging.info(f"Найдено объявлений парсером: {len(soup.select("li.tb-merkflat"))}")
     
